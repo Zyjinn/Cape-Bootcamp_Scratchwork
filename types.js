fe2ced805d875b11f6e7b4578f4316cd3bc7f10b
Zyjin <Xsaber64@gmail.com>
2020-07-14 21:35:49 -0500

    types.tsc finished

--- types.js
+++ types.js
@@ -4,11 +4,27 @@ var myNum;
 var myBool;
 var myVar;
 var strArr;
+var numArr;
+var boolArr;
+// orrrrrrrr
+// let strArr: Array<string>;
+// let numArr: Array<number>;
+// let boolArr: Array<boolean>;
+// Tuples 
+var strNumTuples;
 myString = 'Hello World' + 'MoreStuff';
 myNum = 22;
 myBool = true;
 myVar = true;
 strArr = ['hello', 'boi', 'something'];
+numArr = [1, 2, 3, 4, 5];
+boolArr = [true, false, true];
+strNumTuples = ['hello', 4];
+// misc null types
+var myVoid = null;
+var myNull = undefined;
+var myUndefined = undefined;
+console.log(myVoid);
 console.log(myString);
 console.log(myNum);
 console.log(myBool);
--- types.ts
+++ types.ts
@@ -3,14 +3,39 @@ let myString: string;
 let myNum: number;
 let myBool: boolean;
 let myVar: any;
+
+// Arrays
 let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+
+// orrrrrrrr
+
+// let strArr: Array<string>;
+// let numArr: Array<number>;
+// let boolArr: Array<boolean>;
+
+// Tuples 
+let strNumTuples: [string, number];
 
 myString = 'Hello World' + 'MoreStuff'
 myNum = 22;
 myBool = true;
 myVar = true;
-strArr = ['hello', 'boi', 'something'];
 
+
+strArr = ['hello', 'boi', 'something'];
+numArr = [1, 2, 3, 4, 5];
+boolArr = [true, false, true];
+
+strNumTuples = ['hello', 4];
+
+// misc null types
+let myVoid: void = null;
+let myNull: null = undefined;
+let myUndefined: undefined = undefined;
+
+console.log(myVoid);
 console.log(myString);
 console.log(myNum);
 console.log(myBool);
