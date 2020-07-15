09f402e07744f5d7d7f5d6cf397bab0d297675d4
Zyjin <Xsaber64@gmail.com>
2020-07-14 22:10:27 -0500

    Classes finished

--- classes.js
+++ classes.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // create a class, very much like java!
 var User = /** @class */ (function () {
     // Runs when intantiate obj.
@@ -7,7 +20,28 @@ var User = /** @class */ (function () {
         this.age = age;
         console.log("user Created: " + this.name);
     }
+    User.prototype.register = function () {
+        console.log(this.name + ' is now registered');
+    };
+    User.prototype.payInvoice = function () {
+        console.log(this.name + 'has paid their invoice');
+    };
     return User;
 }());
-var john = new User("John Doe", "Joe@gmail.com", 43);
-console.log(john.age);
+var Member = /** @class */ (function (_super) {
+    __extends(Member, _super);
+    function Member(id, name, email, age) {
+        var _this = _super.call(this, name, email, age) || this;
+        _this.id = id;
+        return _this;
+    }
+    Member.prototype.payInvoice = function () {
+        _super.prototype.payInvoice.call(this);
+    };
+    return Member;
+}(User));
+// let john = new User("John Doe", "Joe@gmail.com", 43);
+// john.register();
+// console.log(john.age);
+var mike = new Member(1, 'Mike Smith', 'Mike@gmail.com', 33);
+mike.payInvoice();
--- classes.ts
+++ classes.ts
@@ -1,8 +1,8 @@
 // create a class, very much like java!
 class User {
-    private name: string;
-    private email: string;
-    private age: number;
+    name: string;
+    email: string;
+    age: number;
 
     // Runs when intantiate obj.
     constructor(name: string, email: string, age: number) {
@@ -12,7 +12,28 @@ class User {
 
         console.log("user Created: " + this.name);
     }
+    register() {
+        console.log(this.name + ' is now registered');
+    }
+    payInvoice() {
+        console.log(this.name + 'has paid their invoice');
+    }
 }
-let john = new User("John Doe", "Joe@gmail.com", 43);
 
-console.log(john.age);
+class Member extends User {
+    id: number;
+    constructor(id: number, name: string, email: string, age: number) {
+        super(name, email, age);
+        this.id = id;
+    }
+    payInvoice() {
+        super.payInvoice();
+    }
+}
+// let john = new User("John Doe", "Joe@gmail.com", 43);
+
+// john.register();
+// console.log(john.age);
+
+let mike: User = new Member(1, 'Mike Smith', 'Mike@gmail.com', 33);
+mike.payInvoice();
