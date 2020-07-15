ab93112a5cd0c4d45b2d471aa03e5b82897bddbd
Zyjin <Xsaber64@gmail.com>
2020-07-14 21:28:03 -0500

    Typescript file added, w/ types declarations

---
+++ types.js
@@ -0,0 +1,15 @@
+// Simple Var declaration
+var myString;
+var myNum;
+var myBool;
+var myVar;
+var strArr;
+myString = 'Hello World' + 'MoreStuff';
+myNum = 22;
+myBool = true;
+myVar = true;
+strArr = ['hello', 'boi', 'something'];
+console.log(myString);
+console.log(myNum);
+console.log(myBool);
+console.log(myVar);
---
+++ types.ts
@@ -0,0 +1,17 @@
+// Simple Var declaration
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+let strArr: string[];
+
+myString = 'Hello World' + 'MoreStuff'
+myNum = 22;
+myBool = true;
+myVar = true;
+strArr = ['hello', 'boi', 'something'];
+
+console.log(myString);
+console.log(myNum);
+console.log(myBool);
+console.log(myVar);
